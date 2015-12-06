5822f7cd732c4e60b30d0b75ab7f7002c05ca85b
oferro <ofer@rotshtein.com>
2015-12-06 02:10:00 +0200

    singl pages

--- js/gezer.js
+++ js/gezer.js
@@ -2,46 +2,33 @@
  * @author oferr
  */
 
-
-
 function smalMenu() {
 	var x = document.getElementById("mnu").style.display;
-	if ( x === "block") {
+	if (x === "block") {
 		document.getElementById("mnu").style.display = "none";
 	} else {
 		document.getElementById("mnu").style.display = "block";
 	}
 }
 
-function sendMail () {
-		var z = document.getElementById("mail-contact");
-//		alert(z.email.value )
-		var email=('ofer.rotshtein@gmail.com');
-
-		var subject = 'From Site:' + z.subject.value;
-	
-	//if (z.frm-email-cc.checked = true){
-	//	var cc = z.email.value
-	//};
-	alert(z.frm-email-cc.checked);
-	
+function sendMail() {
+	var z = document.getElementById("mail-contact");
+	var email = ('ofer.rotshtein@gmail.com');
+	var subject = 'From Site:' + z.subject.value;
+//	if (z.frm-email-cc.checked) {
+//		var cc = z.email.value;
+//	};
 	var body = z.frm-bdy.value;
-	
-/*	document.getElementById("mail").href = 
-		'mailto:' + email + 
-		'?subject=' +subject+
-		'&cc=' +cc+
-		'&body=' +body
-		;
-		alert('mailto:' + email + 
-		'?subject=' +subject+
-		'&cc=' +cc+
-		'&body=' +body');
-*/
-//		">' + 'Click here to send feedback' + '<' + '/a>');
+	document.getElementById("mail").href = 
+	'mailto:' + email +
+	'?subject=' +subject+
+	'&cc=' +cc+
+	'&body=' +body
+	;
+	//		">' + 'Click here to send feedback' + '<' + '/a>');
 }
 
-function printPage () {
+function printPage() {
 	window.print();
 }
 
